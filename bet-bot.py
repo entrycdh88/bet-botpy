ddc28635b517e8fe83272a148a0c55b748edf249
cdh88 <47474750+entrycdh88@users.noreply.github.com>
2019-02-26 15:30:49 +0900

    Update bet-bot.py

--- bet-bot.py
+++ bet-bot.py
@@ -20,9 +20,6 @@ async def on_message(message):
     if message.author.bot:
         return None
 
-    id = message.author.id
-    channel = message.channel
-
     if message.content == '$py':
         await message.channel.send('python')
 
@@ -37,33 +34,5 @@ async def on_message(message):
 
     if message.content == 'zzz':
         await message.channel.send('zzz')
-
-    if message.content == '수류탄':
-        await message.channel.send('킹감자')
-
-    if message.content == '랩터':
-        await message.channel.send('PRO YOUTUBER RAPTORLEE')
-
-    if message.content == '매니저':
-        await message.channel.send('無')
-
-    if message.content == '배그':
-        await message.channel.send('Player Unknown Batle Ground')
-
-    if message.content == '슬찬':
-        await message.channel.send('PRO YOUTUBER RAPTORLEE')
-
-    if message.content == '팬타':
-        await message.channel.send('???: 키킥 넷마블~')
-
-    if message.content == '랩터':
-        await message.channel.send('PRO YOUTUBER RAPTORLEE')
-
-    if message.content == '라면':
-        await message.channel.send('꼬불꼬불 맛좋은 라면~~')
-
-    if message.content == '에임갓':
-        await message.channel.send('반대로 에임ㄸ...')
-
-    
+        
 client.run(token)
